bcbd4b3c907a44e2ec983c05c79bc76e56ea57f8
Kostiantyn Didovych <didovych.k.v@gmail.com>
2026-04-14 16:03:31 +0300

    2.8 Factorial with Recusrion (simple example without validations)

---
+++ src/tasks/2_8/factorialWithRecursion.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let n = prompt('Enter factorial number. (small number like 5)');
+console.log(factorial(n));
+
+/**
+ * Factorial calculation example with recursion
+ *
+ * @param {Number} num 
+ * @returns Number
+ */
+function factorial(n) {
+    if (n <= 1) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
